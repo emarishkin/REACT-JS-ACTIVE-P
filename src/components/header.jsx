3740d15370659fc7cd36f7a04bfa31826fd4652d
eemarishkin <ee.marishkin@yandex.ru>
2024-11-26 21:44:18 +0400

    dobavil-2-buttons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,18 +2,33 @@ import { Button } from "./Button"
 import { useState } from "react"
 
 export function Header(){
-    const [content,setContent] = useState('нажми')
-    
 
-    function handleClick(type){
-        console.log('click',type)
-        setContent(type)
-      
+    let [content,setContent]=useState('нажми сначала на 1 кнопку')
+    let [content2,setContent2]=useState('теперь на 2 кнопку')
+    let [content3,setContent3]=useState('и на меня')
+    
+    function handleclick1(type1){
+        console.log(type1)
+        setContent(type1)
     }
+    function handleclick2(type2){
+        console.log(type2)
+        setContent2(type2)
+    }
+    function handleclick3(type3){
+        console.log(type3)
+        setContent3(type3)
+    }
+    
     return(
         <div className="header">
         <img src="./img/смаилик.jpg" alt="" />
-        <Button onClick={()=>handleClick('не нажимай')}>{content} </Button>
+        <div className="header-button">
+        <Button onClick={()=>handleclick1('поздравляю')}>{content}</Button>
+        <Button onClick={()=>handleclick2('тебя')}>{content2}</Button>
+        <Button onClick={()=>handleclick3('друг')}>{content3}</Button>
+      
+        </div>
         </div>
     )
 }
